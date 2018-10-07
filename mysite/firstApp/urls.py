d73e90a328caed694f9fdbcd58e791e6c48f5611
Apharmacist <896543862@qq.com>
2018-10-08 00:03:34 +0800

    get请求

--- mysite/firstApp/urls.py
+++ mysite/firstApp/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('projects/', views.projects, name='projects'),
-    path('peoples/', views.peoples, name='peoples')
+    path('peoples/', views.peoples, name='peoples'),
+    path('attribles/', views.attribles, name='attribles'),
+    path('get1', views.get1, name='get1'),
+    path('get2', views.get2, name='get2'),
 ]
--- mysite/firstApp/views.py
+++ mysite/firstApp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 from .models import Projects, Peoples
 
 def index(request):
-    return render(request, 'fistApp/index.html')
+    return render(request, 'firstApp/index.html')
 
 
 
@@ -12,9 +13,42 @@ def index(request):
 def projects(request):
     projectList = Projects.objects.all()
     text = {'students': projectList}
-    return render(request, 'firstApp/students.html', text)
+    return render(request, 'firstApp/projects.html', text)
 
 def peoples(request):
     peopleList = Peoples.objects.all()
     text2 = {'students': peopleList}
-    return render(request, 'myApp/students.html', text2)
+    return render(request, 'firstApp/peoples.html', text2)
+
+
+
+def attribles(request):
+    print(request.path)
+    print(request.method)
+    print(request.encoding)
+    print(request.GET)
+    print(request.POST)
+    print(request.FILES)
+    print(request.COOKIES)
+    print(request.SESSION)
+
+    return HttpResponse('TOM is a good boy')
+
+
+# 获取get传递的数据
+
+def get1(request):
+    a = request.GET.get('a')
+    b = request.GET.get('b')
+    c = request.GET.get('c')
+
+    return HttpResponse(a + "" + b + "" + c)
+
+
+def get2(request):
+    a = request.GET.getlist('a')
+    a1 = a[0]
+    a2 = a[1]
+    c = request.GET.get('c')
+
+    return HttpResponse(a1+ "" + a2 +""+ c)
