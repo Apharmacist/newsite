09f092f25a0aa139298c6938fc284d2b5757c9c9
Apharmacist <896543862@qq.com>
2018-10-08 15:56:31 +0800

    GET\POST,session

--- mysite/firstApp/views.py
+++ mysite/firstApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -51,4 +51,96 @@ def get2(request):
     a2 = a[1]
     c = request.GET.get('c')
 
-    return HttpResponse(a1+ "" + a2 +""+ c)
+    return HttpResponse(a1+ "" + a2 + "" + c)
+
+
+
+
+# POST
+
+def showregis(request):
+    return render(request, 'firstApp/register.html')
+
+
+
+def register(request):
+    name = request.POST.get('name')
+    gender = request.POST.get('gender')
+    age = request.POST.get('age')
+    hobby = request.POST.get('hobby')
+    print(name)
+    print(gender)
+    print(age)
+    print(hobby)
+    return HttpResponse('post')
+
+
+# response
+def showresponse(request):
+    res = HttpResponse()
+    res.content = b'good'
+    print(res.content)
+    print(res.charset)
+    print(res.status_code)
+
+
+    return res
+
+
+# cookie
+
+def cookietest(request):
+    res = HttpResponse()
+    cookie = request.COOKIES
+    res.write('<h1>' + cookie['firstApp']+'<h1/>')
+    # cookie = res.set_cookie('firstApp', 'good')
+    return res
+
+
+
+# 重定向
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import redirect
+
+def redirect1(request):
+   # return HttpResponseRedirect('/firstApp /redirect2')
+    return redirect('/firstApp/redirect2')
+
+def redirect2(request):
+
+   # if request.is_ajax():
+   #     a = JsonResponse({})
+
+    return HttpResponse('我是重定向后的视图')
+
+
+
+
+# session
+
+def main(request):
+
+    # 取session
+    username = request.session.get('name', "游客                                 ")
+    return render(request, 'firstApp/main.html', {'username': username})
+
+
+def login(request):
+    return render(request, 'firstApp/login.html')
+
+
+def showmain(request):
+    username = request.POST.get('username')
+
+
+    # 存储session
+    request.session['name'] = username
+    # request.session.set_expiry(10)
+    return redirect('/firstApp/main')
+
+from django.contrib.auth import logout
+def quit(request):
+    # 清除session
+
+    logout(request)
+    return redirect('/firstApp/main')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'XD',
         'USER': 'root',
-        'PASSWORD': 'GJ.580231',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
     }
